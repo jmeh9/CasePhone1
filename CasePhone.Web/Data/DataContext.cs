4acf75c9794d2ad195e0cd44fd2cf57430054afc
Jose Miguel Eliosa Hernandez <mike_super96@hotmail.com>
2019-10-29 10:28:27 -0600

    Crear DataContext y Entities

---
+++ CasePhone.Web/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using CasePhone.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasePhone.Web.Data
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
