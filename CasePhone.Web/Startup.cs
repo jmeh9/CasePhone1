fb59b4e6ff6916f0cb47644b71d5ae1ec35d6f22
UVM-PUEBLA <UVM-PUEBLA@DESKTOP-5VJHBI8>
2019-10-30 17:28:21 -0600

    base de datos

--- CasePhone.Web/Startup.cs
+++ CasePhone.Web/Startup.cs
@@ -24,15 +24,15 @@ namespace CasePhone.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
+            services.AddDbContext<DataContext>(cfg =>
+
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+
             });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+           
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
