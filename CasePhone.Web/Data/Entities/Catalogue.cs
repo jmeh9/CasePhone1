9b4821e09ca617a7516b5ebc626332a55bbbaada
Jose Miguel Eliosa Hernandez <mike_super96@hotmail.com>
2019-10-30 17:15:49 -0600

    Creación clases

---
+++ CasePhone.Web/Data/Entities/Catalogue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasePhone.Web.Data.Entities
+{
+    public class Catalogue : IEntity
+    {
+        public int Id { get; set; }
+        [Display(Name = "Catalogo")]
+        public string Cellphonecover { get; set; }
+    }
+}
---
+++ CasePhone.Web/Data/Entities/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasePhone.Web.Data.Entities
+{
+    public class Contact:IEntity
+    {
+        public int Id { get; set; }
+        [Display (Name="Contacto")]
+        public int NumberCellphone { get; set; } 
+    }
+}
