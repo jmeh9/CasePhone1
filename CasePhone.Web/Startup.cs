358b08f83f59c64884ccd02d9d264e5b418795a2
JOCELYN MEDINA <jocesimpson@hotmail.com>
2019-10-31 18:16:37 -0600

    Se agregaron cambios

--- CasePhone.Web/Startup.cs
+++ CasePhone.Web/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CasePhone.Web.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -27,9 +29,7 @@ namespace CasePhone.Web
             services.AddDbContext<DataContext>(cfg =>
 
             {
-
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-
             });
            
             services.AddMvc();
