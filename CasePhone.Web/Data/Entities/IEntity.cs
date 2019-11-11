583b263abaa43fc49532883094f5a3fb885b1bc2
Jose Miguel Eliosa Hernandez <mike_super96@hotmail.com>
2019-11-11 16:27:18 -0600

    public Entities

--- CasePhone.Web/Data/Entities/IEntity.cs
+++ CasePhone.Web/Data/Entities/IEntity.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CasePhone.Web.Data.Entities
+﻿namespace CasePhone.Web.Data.Entities
 {
-    interface IEntity
+    public interface IEntity
     {
         int Id { get; set; }
     }
